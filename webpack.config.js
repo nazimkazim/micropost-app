0a9820f3c19fea89c3e1e936891dc5e37bc2edf9
Your Name <nazik_111@mail.ru>
2018-04-17 13:12:42 +0500

    change resolve to join in webback.config file

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
     filename: 'app.bundle.js',
     publicPath: '/static/'
   },
